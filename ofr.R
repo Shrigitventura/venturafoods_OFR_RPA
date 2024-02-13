930afa4b379092b08ec11f82266cf117f0d451c9
slee <sanlee8495@gmail.com>
2024-02-13 11:34:55 -0800

    02122024

--- ofr.R
+++ ofr.R
@@ -12,8 +12,8 @@ library(rio)
 
 ### Daily Processing ###
 #################################################################### Read Files ####################################################################
-ofr <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/OFR/Daily Updates/2024/02.09.2024/ofr.xlsx")
-csv_data <- read_csv("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/OFR/Daily Updates/2024/02.09.2024/csv.CSV")
+ofr <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/OFR/Daily Updates/2024/02.12.2024/ofr.xlsx")
+csv_data <- read_csv("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/OFR/Daily Updates/2024/02.12.2024/csv.CSV")
 ####################################################################################################################################################
 
 # Clean Data
