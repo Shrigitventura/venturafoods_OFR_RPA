84487d6b37bba0d510e6be9003214a81e4db30bb
slee <sanlee8495@gmail.com>
2025-01-08 11:05:45 -0800

    01082025

---
+++ date_fix.R
@@ -0,0 +1,8 @@
+readRDS("OFR_data_base.rds") -> date_fix
+
+date_fix$shortage_date %>% unique()
+date_fix %>% 
+  dplyr::mutate(shortage_date = as.character(shortage_date)) %>% 
+  dplyr::filter(shortage_date != "4755239-06-28") -> date_fix
+
+saveRDS(date_fix, "OFR_data_base.rds")
