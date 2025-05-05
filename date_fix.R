61f7467286fb14d6bca9e717518651375559ea2a
Shrigitventura <SPoudel@venturafoods.com>
2025-05-05 10:19:30 -0400

    4/26/2025

--- date_fix.R
+++ date_fix.R
@@ -7,6 +7,6 @@ date_fix$shortage_date %>% unique()
 #####################################################################
 date_fix %>% 
   dplyr::mutate(shortage_date = as.character(shortage_date)) %>% 
-  dplyr::filter(shortage_date != "4777782-03-27") -> date_fix
+  dplyr::filter(shortage_date != "4780384-05-05") -> date_fix
 
 saveRDS(date_fix, "OFR_data_base.rds")
